49e8acc2f62d596f87ad3409dfef09d65e2f8778
wimgonza <wimgonza@ucsc.edu>
2024-11-11 14:34:15 -0800

    L16 Dialog (Setup)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 // todo
+
+const button = document.createElement("button");
+button.textContent = "I am a button.";
+
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+document.body.appendChild(button);
